2b877d162a06438aba20cecac616d8382c8021f7
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-08 18:31:24 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,17 +1,30 @@
-variable "region" {
-  type    = string
-  default = "ap-south-1"
-}
-
 variable "vpc_id" {
-  type = string
-}
-
-variable "subnet_id" {
-  type = string
+  description = "The VPC ID where resources will be created"
+  type        = string
 }
 
 variable "ami" {
-  type = string
+  description = "AMI ID for EC2 instance"
+  type        = string
 }
 
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be launched"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability zones list"
+  type        = list(string)
+}
