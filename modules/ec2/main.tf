52b72a5a503c9ede58d7830c3535ebaef38c944f
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-09 11:06:55 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,12 +3,12 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_name
 
   tags = merge(
     {
-      Name = "${var.name}"
+      Name = var.name
     },
     var.tags
   )
 }
-
