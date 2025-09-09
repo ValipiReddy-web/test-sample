1ac524748e5a202fc1ee190954e22f02c7df2114
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-09 11:07:18 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,27 +1,37 @@
 variable "name" {
-  type = string
+  description = "Name of the EC2 instance"
+  type        = string
 }
 
 variable "ami" {
-  type = string
+  description = "AMI ID for the EC2 instance"
+  type        = string
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "subnet_id" {
-  type = string
+  description = "Subnet ID for the EC2 instance"
+  type        = string
 }
 
 variable "security_group_ids" {
-  type    = list(string)
-  default = []
+  description = "List of security group IDs to attach"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Optional tags"
+  type        = map(string)
+  default     = {}
 }
-
